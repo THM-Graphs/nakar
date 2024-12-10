fe6178dc2b3b66f29223eb987464a53ff96024ff
Samuel Schepp <sa.schepp@googlemail.com>
2024-12-10 15:18:41 +0100

    adds basic vision pro client

---
+++ nakar-client-vision/nakar-client-vision/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nakar-client-vision
+//
+//  Created by Samuel Schepp on 10.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ nakar-client-vision/nakar-client-vision/nakar_client_visionApp.swift
@@ -0,0 +1,17 @@
+//
+//  nakar_client_visionApp.swift
+//  nakar-client-vision
+//
+//  Created by Samuel Schepp on 10.12.24.
+//
+
+import SwiftUI
+
+@main
+struct nakar_client_visionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
