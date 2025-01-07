38396452eaf508a683e47660a183049e13616c02
Samuel Schepp <sa.schepp@googlemail.com>
2025-01-07 01:11:43 +0100

    adds healthchecks to docker

--- nakar-server/config/server.ts
+++ nakar-server/config/server.ts
@@ -2,8 +2,8 @@ import { Env } from '@strapi/utils/dist/env-helper';
 
 export default ({ env }: { env: Env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('URL', 'http://localhost:1337'),
+  port: env.int('PORT', 80),
+  url: env('URL', 'http://localhost:80'),
   app: {
     keys: env.array('APP_KEYS'),
   },
