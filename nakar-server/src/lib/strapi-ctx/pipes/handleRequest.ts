6b6c0660757f93746563626ab79abd033a66a17a
Samuel Schepp <sa.schepp@googlemail.com>
2025-01-18 01:00:14 +0100

    dont send 400 on neo4j error

--- nakar-server/src/lib/strapi-ctx/pipes/handleRequest.ts
+++ nakar-server/src/lib/strapi-ctx/pipes/handleRequest.ts
@@ -1,7 +1,6 @@
 import { Context } from 'koa';
 import { match, P } from 'ts-pattern';
-import { Neo4jError } from 'neo4j-driver';
-import { BadRequest, HttpError, InternalServerError } from 'http-errors';
+import { HttpError, InternalServerError } from 'http-errors';
 import { handleError } from './handleError';
 
 export function handleRequest<T>(
@@ -14,17 +13,11 @@ export function handleRequest<T>(
       return ctx;
     } catch (unknownError: unknown) {
       return match(unknownError)
-        .with(P.instanceOf(Neo4jError), (error) =>
-          handleError(ctx, new BadRequest(error.message)),
-        )
         .with(P.instanceOf(HttpError), (error) => handleError(ctx, error))
         .with(P.instanceOf(Error), (error) =>
           handleError(ctx, new InternalServerError(error.message)),
         )
         .otherwise((error) => {
-          console.debug(
-            `Unknown error in route handle: ${JSON.stringify(error)}`,
-          );
           return handleError(
             ctx,
             new InternalServerError(`Unknown error: ${JSON.stringify(error)}`),
