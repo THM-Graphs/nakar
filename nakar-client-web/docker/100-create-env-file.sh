45d1a37624c8c022a88270b341f83d41f311b66d
Samuel Schepp <sa.schepp@googlemail.com>
2025-01-31 19:36:29 +0100

    docker: fix BACKEND_SOCKET_URL write

--- nakar-client-web/docker/100-create-env-file.sh
+++ nakar-client-web/docker/100-create-env-file.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "BACKEND_URL=$BACKEND_URL" >> /usr/share/nginx/html/.env
+echo "BACKEND_SOCKET_URL=$BACKEND_SOCKET_URL" >> /usr/share/nginx/html/.env
 cat /usr/share/nginx/html/.env
