5f5c07864d8c0352bd3f94400488faa355b68e0e
Samuel Schepp <sa.schepp@googlemail.com>
2025-03-14 11:53:35 +0100

    Revert "uses dist build path for room worker launch script"
    This reverts commit 79e8a87f5c329cfc50a6fccf5552e4e978c70e20.

--- nakar-server/src/lib/services/room/RoomService.ts
+++ nakar-server/src/lib/services/room/RoomService.ts
@@ -22,7 +22,6 @@ import { WTEvent } from '../room-instance/worker-events/WTEvent';
 import { match } from 'ts-pattern';
 import { WTEventPhysicsUpdate } from '../room-instance/worker-events/WTEventPhysicsUpdate';
 import z from 'zod';
-import os from 'node:os';
 
 export class RoomService implements ApplicationService {
   private readonly _workers: SMap<string, Worker>;
@@ -216,15 +215,7 @@ export class RoomService implements ApplicationService {
           graph: plainGraph,
         };
         const worker: Worker = new Worker(
-          path.join(
-            process.cwd(),
-            'dist',
-            'src',
-            'lib',
-            'services',
-            'room',
-            'RoomWorker.js',
-          ),
+          path.join(__dirname, 'RoomWorker.js'),
           {
             workerData: workerData,
           },
