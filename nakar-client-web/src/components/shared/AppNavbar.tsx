c8164b3ad1798dff4bb64b66775c83e1818fb6eb
Samuel Schepp <sa.schepp@googlemail.com>
2025-06-11 09:49:32 +0200

    fixes navbar on small widths

--- nakar-client-web/src/components/shared/AppNavbar.tsx
+++ nakar-client-web/src/components/shared/AppNavbar.tsx
@@ -15,22 +15,27 @@ export function AppNavbar(props: {
         height: "32px",
       }}
     >
-      <Stack direction={"horizontal"} className={"align-items-stretch"} gap={5}>
-        {props.left}
-      </Stack>
-      <Stack
-        direction={"horizontal"}
-        gap={2}
-        className={
-          "ps-1 pe-1 position-absolute justify-content-center align-content-center"
-        }
-        style={{ width: "100%", height: "100%", pointerEvents: "none" }}
-      >
-        <Stack direction={"horizontal"} gap={1}>
-          <Image alt="" src="/logo.png" width="20" height="20" roundedCircle />
-          <span className={"small fw-bold"}>NAKAR</span>
+      <Stack direction={"horizontal"} gap={5}>
+        <Stack
+          direction={"horizontal"}
+          className={"align-items-stretch"}
+          gap={5}
+        >
+          {props.left}
         </Stack>
-        {props.center}
+        <Stack direction={"horizontal"} gap={2}>
+          <Stack direction={"horizontal"} gap={1}>
+            <Image
+              alt=""
+              src="/logo.png"
+              width="20"
+              height="20"
+              roundedCircle
+            />
+            <span className={"small fw-bold"}>NAKAR</span>
+          </Stack>
+          {props.center}
+        </Stack>
       </Stack>
       <Stack direction={"horizontal"} className={"align-items-stretch"} gap={5}>
         {props.right}
