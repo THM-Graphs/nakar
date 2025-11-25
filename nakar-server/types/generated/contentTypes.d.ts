44ef23b9eecad6481ff6cfedba12cc19347b6b0f
Samuel Schepp <sa.schepp@googlemail.com>
2025-11-25 17:42:35 +0100

    fixes bug that note creation would fail on to many nodes

--- nakar-server/types/generated/contentTypes.d.ts
+++ nakar-server/types/generated/contentTypes.d.ts
@@ -421,7 +421,7 @@ export interface ApiNoteNote extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    elementIds: Schema.Attribute.String;
+    elementIds: Schema.Attribute.Text;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::note.note'> &
       Schema.Attribute.Private;
