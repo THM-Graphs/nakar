24c63a070b682b05bda0a2272df523138553d1be
Samuel Schepp <sa.schepp@googlemail.com>
2026-02-11 18:42:04 +0100

    improves padding of scenario panel

--- nakar-client-web/src/room/scenarios-panel/ScenarioGroupList.tsx
+++ nakar-client-web/src/room/scenarios-panel/ScenarioGroupList.tsx
@@ -1,25 +1,33 @@
 import { ScenarioGroupDisplay } from "./ScenarioGroupDisplay.tsx";
 import { Stack } from "react-bootstrap";
 import { ScenarioGroupDto } from "../../../src-gen";
+import { Fragment } from "react";
 
 export function ScenarioGroupList(props: {
   scenarioGroups: ScenarioGroupDto[];
 }) {
   return (
-    <Stack className={"flex-grow-0 pb-5"}>
+    <>
       {props.scenarioGroups.length == 0 && (
         <span
-          className={"small text-muted ms-1 fst-italic align-self-center pb-1"}
+          className={
+            "small text-muted ms-1 fst-italic align-self-center pb-5 pt-5"
+          }
         >
           empty
         </span>
       )}
-      {props.scenarioGroups.map((scenarioGroup: ScenarioGroupDto) => (
-        <ScenarioGroupDisplay
-          key={scenarioGroup.id}
-          scenarioGroup={scenarioGroup}
-        ></ScenarioGroupDisplay>
-      ))}
-    </Stack>
+      {props.scenarioGroups.length > 0 && (
+        <Stack className={"flex-grow-0 pb-5"}>
+          {props.scenarioGroups.map((scenarioGroup: ScenarioGroupDto) => (
+            <Fragment key={scenarioGroup.id}>
+              <ScenarioGroupDisplay
+                scenarioGroup={scenarioGroup}
+              ></ScenarioGroupDisplay>
+            </Fragment>
+          ))}
+        </Stack>
+      )}
+    </>
   );
 }
