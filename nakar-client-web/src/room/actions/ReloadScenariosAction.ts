dfd28d197305d0e1e6275c46df25118d26fc023d
Samuel Schepp <sa.schepp@googlemail.com>
2026-03-17 14:23:57 +0100

    implements reload scenarios action

---
+++ nakar-client-web/src/room/actions/ReloadScenariosAction.ts
@@ -0,0 +1,44 @@
+import { Action } from "./Action.ts";
+import { CanvasContextData } from "../../pages/Canvas.tsx";
+import {
+  CanvasPageDto,
+  publicCanvasControllerGetCanvas,
+} from "../../../src-gen";
+import { resultOrThrow } from "../../shared/data/resultOrThrow.ts";
+import { useBearStore } from "../../state/useBearStore.ts";
+
+export type ReloadScenariosActionParams = {
+  roomContext: CanvasContextData;
+};
+
+export class ReloadScenariosAction extends Action<ReloadScenariosActionParams> {
+  public static shared: ReloadScenariosAction = new ReloadScenariosAction();
+
+  protected async action(props: ReloadScenariosActionParams): Promise<void> {
+    const data: CanvasPageDto = resultOrThrow(
+      await publicCanvasControllerGetCanvas({
+        path: {
+          canvasId: props.roomContext.initialCanvasData.id,
+          roomId: props.roomContext.initialRoomData.id,
+        },
+      }),
+    );
+    useBearStore.getState().room.panels.scenarios.setScenarios(data.scenarios);
+  }
+
+  disabled(): boolean {
+    return false;
+  }
+
+  icon(): string | null {
+    return "arrow-clockwise";
+  }
+
+  slug(): string {
+    return "reload-scenarios";
+  }
+
+  title(p: ReloadScenariosActionParams): string {
+    return "Reload Scenarios";
+  }
+}
--- nakar-client-web/src/room/scenarios-panel/ScenariosPanel.tsx
+++ nakar-client-web/src/room/scenarios-panel/ScenariosPanel.tsx
@@ -2,13 +2,28 @@ import { Panel } from "../../shared/elements/Panel.tsx";
 import { useBearStore } from "../../state/useBearStore.ts";
 import { ScenarioGroupList } from "./ScenarioGroupList.tsx";
 import { Stack } from "react-bootstrap";
+import { ActionNavbarButton } from "../actions/ActionNavbarButton.tsx";
+import { ReloadScenariosAction } from "../actions/ReloadScenariosAction.ts";
+import { useCanvasContext } from "../../pages/Canvas.tsx";
 
 export function ScenariosPanel() {
   const hide = useBearStore((s) => s.room.panels.scenarios.hide);
   const scenarios = useBearStore((s) => s.room.panels.scenarios.scenarios);
+  const roomContext = useCanvasContext();
 
   return (
-    <Panel direction={"left"} onClose={hide} title={"Scenarios"}>
+    <Panel
+      direction={"left"}
+      onClose={hide}
+      title={"Scenarios"}
+      toolbar={
+        <ActionNavbarButton
+          action={ReloadScenariosAction.shared}
+          params={{ roomContext: roomContext }}
+          hideTitle={true}
+        ></ActionNavbarButton>
+      }
+    >
       <Stack className={"pb-5 overflow-y-scroll"}>
         <ScenarioGroupList
           scenarioGroups={scenarios.scenarioGroups}
