d547ec00753571922e32b346fa6e2ec3f4e5ed71
Samuel Schepp <sa.schepp@googlemail.com>
2026-06-02 15:35:46 +0200

    fixes bug that would fail initial load of client

--- nakar-client-web/vite.config.ts
+++ nakar-client-web/vite.config.ts
@@ -4,4 +4,11 @@ import viteReact from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [viteReact()],
+  build: {
+    rolldownOptions: {
+      output: {
+        strictExecutionOrder: true,
+      },
+    },
+  },
 });
