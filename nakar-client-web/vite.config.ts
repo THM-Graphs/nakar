46ded1f31b768fead304488144d3653d8c3a0101
Samuel Schepp <sa.schepp@googlemail.com>
2026-06-04 14:34:40 +0200

    reworks structure of client packages

--- nakar-client-web/vite.config.ts
+++ nakar-client-web/vite.config.ts
@@ -4,6 +4,9 @@ import viteReact from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [viteReact()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   build: {
     rolldownOptions: {
       output: {
