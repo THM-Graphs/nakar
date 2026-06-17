3ef73b992311a16804563b90add449c59d010e3a
Samuel Schepp <sa.schepp@googlemail.com>
2026-06-17 10:03:11 +0200

    do not start nest on maintenance modes

--- nakar-server/src/index.ts
+++ nakar-server/src/index.ts
@@ -16,6 +16,19 @@ export default {
    * run jobs, or perform some special logic.
    */
   async bootstrap(): Promise<void> {
+    strapi.log.debug(`Arguments: ${process.argv.join(' ')}`);
+
+    // Do not start the nest application if strapi is started with maintenance flags
+    const maintenanceFlags: string[] = ['transfer', 'import', 'export'];
+    for (const maintenanceFlag of maintenanceFlags) {
+      if (process.argv.includes(maintenanceFlag)) {
+        strapi.log.info(
+          `Will not bootstrap nest application, because strapi is started with ${maintenanceFlag} flag.`,
+        );
+        return;
+      }
+    }
+
     await bootstrapNest();
   },
 
